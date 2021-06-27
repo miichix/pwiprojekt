4b8c64eb935a2e4b30cf7c0c8acbca93a67acd72
Michał Tański <miner.com6@gmail.com>
2021-06-28 00:15:52 +0200

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PwI_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Strona opisowa.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Strona konktaktowa.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PwI_MVC.Models;
+using PwI_MVC.ViewModels;
+
+namespace PwI_MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1 "},
+                new Customer {Name = "Customer 2"}
+            };
+
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+           
+
+        }
+
+    }
+
+
+}
